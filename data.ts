570c8385918df0f332a28e3ded20dedee25dcec4
satriadhm <glorioussatria@gmail.com>
2025-11-03 23:55:05 +0700

    fix

--- data.ts
+++ data.ts
@@ -7,11 +7,11 @@ export const messageData = {
       
       "Seiring dengan itu, I want to tell you something that’s been buried for quite a long time. Believe me, this won’t be a romantic thing (I hope). Aku gak tahu siapa aku sebenarnya di matamu. Maybe in your eyes, I’m just a friend, or maybe just someone you once knew. But for me, it was different.",
       
-      "Aku sangat ingin berterima kasih atas peranmu di hidupku tujuh tahun yang lalu. Your presence really opened my eyes. I didn’t really have any role models until I met you and your family. I always looked up to you, your father, and your brother. Even until today, I still admire your brother’s handcrafts they’re brilliant, even from a computer science student’s perspective. You all shaped a vision in me that I never had before.",
+      "Aku sangat ingin berterima kasih atas peranmu di hidupku tujuh tahun yang lalu. Your presence really opened my eyes. I didn’t really have any role models until I met you and your family. I always looked up to you, your father, and your brother. Even until today, I still admire your brother’s handcrafts. They’re brilliant, even from a computer science student’s perspective. You all shaped a vision in me that I never had before.",
       
-      "It was you who showed me hope through education and studying. Maybe you don’t remember, but it was you who once believed that one day I would study abroad. I did not at Oxford, I’m sorry 😄 (maybe someday? even if just visiting, haha). It was you who encouraged me to do better every day. You made me care about my grades, even when I was surrounded by bad influences back then. You know very well from whom those influences came. But in the end, my SMP national exam grades weren’t that bad.",
+      "It was you who showed me hope through education and studying. Maybe you don’t remember, but it was you who once believed that one day I would study abroad. I DID! But not at Oxford, I’m sorry 😄 (maybe someday? even if just visiting, haha). It was you who encouraged me to do better every day. You made me care about my grades, even when I was surrounded by bad influences back then. You know very well from whom those influences came. But in the end, my SMP national exam grades weren’t that bad.",
       
-      "Maybe I can say that if we had never met seven years ago, I wouldn’t be as successful as I am today. One thing leads to another good grades in high school led me to a better environment, and a good environment shaped me into a better person eventually.",
+      "Maybe I can say that if we had never met seven years ago, I wouldn’t be as successful as I am today. One thing leads to another, good grades in high school led me to a better environment, and a good environment shaped me into a better person eventually.",
       
       "I was just a fragile boy back then. Aku sadar sekali aku melakukan kesalahan, kita berpisah, dan aku sempat jatuh. I spent a year and a half in high school just trying to get attention, hoping you’d see me fall by being rude and chaotic. My dumb self thought you needed to see my downfall. Aku tahu itu hal yang bodoh, but I was too much of a coward to face it all, and tried to cover my true self by being a completely different person.",
       
@@ -19,11 +19,11 @@ export const messageData = {
       
       "We never really had proper closure, I guess. Tapi wajar, karena kita juga tidak memulainya dengan proper (I was too coward that I asked you out via WhatsApp 😄). Yes, I was a coward, and probably still was until we graduated from Smanssa. But I learned at least from my previous mistakes.",
       
-      "I know very well that our paths won’t cross again like before. Even if they do, I’m not sure things would ever be the same. But still, I want to thank you again unconsciously, you did a great job playing your part in my life. I believe that if I didn’t explain all this, you’d never realize it, because I kept it all in the deepest part of my heart.",
+      "I know very well that our paths won’t cross again like before. Even if they do, I’m not sure things would ever be the same. But still, I want to thank you again. Unconsciously, you did a great job playing your part in my life. I believe that if I didn’t explain all this, you’d never realize it, because I kept it all in the deepest part of my heart.",
       
-      "Honestly, I don’t know how you feel about me after all these years. But it’s okay it doesn’t matter. We both have our own lives now, and I’m here just wishing you all the best in your life, today and tomorrow.",
+      "Honestly, I don’t know how do you feel about me after all these years. But it’s okay it doesn’t matter. We both have our own lives now, and I’m here just wishing you all the best in your life, today and tomorrow.",
       
-      "Promise me you won’t act differently once you find out who wrote this. Think of this message as something I sent through a time machine, from my future self, at the right moment. Now, I just want to be your friend. Karena ya, sepertinya kita gak sejauh itu we were once classmates, and even went to college in the same city, even though you said we never met.",
+      "Promise me you won’t act differently once you find out who wrote this. Think of this message as something I sent through a time machine, from my future self, at the right moment. Now, I just want to be your friend. Karena ya, sepertinya kita gak sejauh itu, we were once classmates, and even went to college in the same city, even though you said we never met.",
       
       "Keep being the best version of yourself.",
     ],
